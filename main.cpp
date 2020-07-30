b4d05090801044c15e4547510e0f5699b05508bc
Nathanael Wettstein <nathanael.wettstein@gmail.com>
2020-07-30 20:38:42 +0200

    Add verbose (-v) option.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <cstring>
+#include <chrono>
+#include <thread>
 #include "parser.hpp"
 #include "slicer.hpp"
 #include "solver.hpp"
@@ -10,23 +12,22 @@
 /**
  * Apply all solvers to all slices.
  *
- * This method loops as long as some progress is made. A maximum of N*N*N = 729 loops
- * are needed if we assume that, in each loop, at least one bit changes.
+ * This method loops as long as some progress is made. A maximum of
+ * N*N*N = 729 loops are needed if we assume that - in each loop - at
+ * least one bit changes.
  */
-bool solveBoard(mask *board) {
+bool solveBoard(mask *board, bool isVerbose) {
     int maxLoops = N * N * N;
     int infoLoops = 1;
     int solverLength = 2;
     int slicerLength = 3;
     SolverInterface *solver[solverLength];
-    SlicerInterface *slicer[slicerLength];
     solver[0] = new DetermineSolver();
     solver[1] = new EliminateSolver();
+    SlicerInterface *slicer[slicerLength];
     slicer[0] = new HorizontalSlicer(board);
     slicer[1] = new VerticalSlicer(board);
     slicer[2] = new BoxSlicer(board);
-    // some sudokus don't implement the diagonal rule, so let's not use DiagonalSlicer
-    // slicer[3] = new DiagonalSlicer(board);
     int numLoops;
     for (numLoops = 0; numLoops < maxLoops; numLoops++) {
         for (int i = 0; i < solverLength; i++) {
@@ -35,22 +36,34 @@ bool solveBoard(mask *board) {
                 slicer[j]->reset();
             }
         }
-        ++numLoops;
-        if (numLoops % infoLoops == 0) {
-            std::cout << numLoops << " loops and counting ..." << std::endl;
-            std::cout << Visualizer::printBoard(board) << std::endl;
-        }
         if (SolverInterface::isBoardSolved(board)) {
             break;
         }
+        if (isVerbose and (numLoops % infoLoops == 0)) {
+            std::cout << "\n\n" << std::endl;
+            std::cout << "Loop " << (numLoops + 1) << " ..." << std::endl;
+            std::cout << Visualizer::printBoard(board) << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+            std::cout.flush();
+        }
     }
-    std::cout << "Done after " << numLoops << " loops." << std::endl;
+    std::cout << "Done after " << (numLoops + 1) << " loops." << std::endl;
     return true;
 }
 
 
 int main(int argc, char **argv) {
-    std::cout << "simple sudoku solver, v 0.0.3" << std::endl;
+    std::cout << "simple sudoku solver, v 0.0.4" << std::endl;
+    bool isVerbose = false;
+    if (argc > 1) {
+        if (strcmp(argv[1], "-v") == 0 or strcmp(argv[1], "--verbose") == 0) {
+            isVerbose = true;
+        }
+        else {
+            std::cout << "Usage: ./sudoku [-v]" << std::endl;
+            exit(-1);
+        }
+    }
     std::string inputString;
     std::string tmpString;
     for (int i = 0; i < N; i++) {
@@ -70,7 +83,7 @@ int main(int argc, char **argv) {
     std::cout << Visualizer::printBoard(board) << std::endl;
 
     std::cout << "Solving board ..." << std::endl;
-    solveBoard(board);
+    solveBoard(board, isVerbose);
     int globalResult = SolverInterface::isBoardSolved(board);
     int returnValue;
     if (globalResult) {
